7dee7ba92a454bf7feb1b672216a99e2649f58a4
kaz94 <k.rutkowska94@gmail.com>
2016-04-07 22:52:08 +0200

    Błąd przy neiktórych wywołaniach

---
+++ src/N.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Kasia on 07.04.16.
+ */
+public class N {
+    public static void main(String[] args)
+    {
+
+        for (double i = 0.0; i<0.4; i+= 0.02){
+           // System.out.println(String.format("%.2f",i));
+            Execution e = new Execution(i);
+        }
+
+
+    }
+}
